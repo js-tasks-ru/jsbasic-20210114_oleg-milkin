d91d3991fcdbcca5386217d513cdc62778e9f2bb
OlegMilkin <olegek.milkin@gmail.com>
2021-01-19 14:17:38 +0200

    Task 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let result = n || 1;
+
+  for (let i = n; i > 1; i--) {
+    result = result * (i - 1);
+  }
+
+  return result;
 }
+
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
