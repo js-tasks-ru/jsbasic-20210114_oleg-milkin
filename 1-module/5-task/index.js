084bc588fca786a614b572769697151659734ba6
OlegMilkin <olegek.milkin@gmail.com>
2021-01-21 00:31:16 +0200

    module 1 task5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,14 @@
  */
 function truncate(str, maxlength) {
   // ваш код...
+  let strLength = str.length;
+
+  if (strLength > maxlength) {
+
+    let lastLetterIndex = maxlength - 1;
+
+    return str.substr(0, lastLetterIndex) + '…';
+  }
+
+  return str;
 }
