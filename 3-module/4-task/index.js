e74438f7de9f230b6631d47e6fb71852fb65f5a4
OlegMilkin <olegek.milkin@gmail.com>
2021-01-31 00:39:36 +0200

    module 3 task4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -6,4 +6,18 @@
  */
 function showSalary(users, age) {
   // ваш код...
+  let filteredUsers = users.filter(user => user.age <= age);
+  let result = '';
+
+  filteredUsers.forEach((item, index) => {
+
+    if (index === filteredUsers.length - 1) {
+      result += `${item.name}, ${item.balance}`;
+    } else {
+      result += `${item.name}, ${item.balance}\n`;
+    }
+    return result;
+  });
+
+  return result;
 }
