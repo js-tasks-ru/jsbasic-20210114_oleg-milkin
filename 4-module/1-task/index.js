6535bea0ea1da54d5faff064e8ce7162d524e083
OlegMilkin <olegek.milkin@gmail.com>
2021-02-01 01:04:15 +0200

    module 4 task1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,4 +5,14 @@
  */
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement('ul');
+
+  friends.forEach(function(item){
+    let li = document.createElement('li');
+    li.innerHTML = item.firstName + item.lastName;
+    ul.append(li);
+  });
+
+  return ul;
 }
+
