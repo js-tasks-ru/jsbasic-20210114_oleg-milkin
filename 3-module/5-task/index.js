70ea6f4e1f6b630fabec96c99888a13244e95542
OlegMilkin <olegek.milkin@gmail.com>
2021-01-31 19:18:56 +0200

    module 3 task5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,18 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+
+function isNumber(value) {
+  return typeof (value) === 'number' && isFinite(value);
+}
+
 function getMinMax(str) {
   // ваш код...
+  let arrStr = str.split(' ').join().split(',');
+  let numbers = arrStr.filter(item => isNumber(+item) && item !== "");
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  };
 }
