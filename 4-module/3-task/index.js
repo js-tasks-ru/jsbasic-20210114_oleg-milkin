af7ff3848da8907a0e9f0bb637f65f0911f1f835
OlegMilkin <olegek.milkin@gmail.com>
2021-02-02 19:46:21 +0200

    4-module, 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,31 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+
+    let statusList = table.rows[i].cells[3].dataset.available;
+    let genderList = table.rows[i].cells[2];
+    let ageList = table.rows[i].cells[1];
+
+    //Available/unavailable class set
+    if (statusList === 'true') {
+      table.rows[i].classList.add('available');
+    } else if (statusList === 'false') {
+      table.rows[i].classList.add('unavailable');
+    } else {
+      table.rows[i].hidden = true;
+    }
+
+    //Gender class set
+    if (genderList.innerText === 'm') {
+      genderList.classList.add('male');
+    } else {
+      genderList.classList.add('female');
+    }
+
+    //Age styling
+    if (ageList.innerText < 18) {
+      ageList.style.textDecoration = 'line-through';
+    }
+  }
 }
