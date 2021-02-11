2bcfef2c0668f9cac529dbbdb2e9394f5557448c
OlegMilkin <olegek.milkin@gmail.com>
2021-02-11 16:17:49 +0200

    6-module, 2-task fix

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -21,12 +21,10 @@ export default class ProductCard {
     this.elem.innerHTML = template;
 
     this.elem.addEventListener('click', (e) => {
-      if (e.target.tagName === 'IMG' && e.target.closest('.card__button')) {
+      if (e.target.closest('.card__button')) {
 
         let myEvent = new CustomEvent("product-add", {
-          detail: {
-            id: product.id,
-          },
+          detail: product.id,
           bubbles: true
         });
 
